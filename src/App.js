35b08d158b1e7a72275736cf8aed288686448c78
Konstantin <kostik-kostik-12@mail.ru>
2024-09-13 20:50:34 +0300

    add works

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
-import CommentsList from "./components/Commentslist";
-import Message from "./components/Message";
-import TemperatureConverter from "./components/TemperatureConverter";
-import TodoList from "./components/TodoList";
+import Message from "./components/hw1/Message";
+
+import CommentsList from "./components/hw2/Commentslist";
+
+import TemperatureConverter from "./components/hw3/TemperatureConverter";
+import TodoList from "./components/hw3/TodoList";
+
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import HomePage from './components/hw4/HomePage';
+import AboutPage from './components/hw4/AboutPage';
 
 function App() {
 	return (
@@ -19,6 +25,28 @@ function App() {
 			<p className="less">Домашнее задание 3</p>
 			<TemperatureConverter/>
 			<TodoList/>
+			<br></br>
+
+			<p className="less">Домашнее задание 4</p>
+			<Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Главная страница</Link>
+            </li>
+            <li>
+              <Link to="/about">О нас</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </div>
+    </Router>
 		</div>
 	);
 }
